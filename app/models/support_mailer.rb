219f3e6c37a6e869774e02c54f7e26c2e70d4594
Michael Maloney <maloney.mc@gmail.com>
2010-07-06 17:27:51 -0400

    adding mailer to support page

--- app/models/support_mailer.rb
+++ app/models/support_mailer.rb
@@ -1,4 +1,10 @@
 class SupportMailer < ActionMailer::Base
-  
+  def support_message(user_name, user_email, user_subject, message_body)
+    recipients "maloney.mc@gmail.com"
+    sent_on Time.now
+    from "support_robot@getmonsterguard.com"
+    subject "MESSAGE FROM MONSTERGUARD SUPPORT PAGE"
+    body :user_name => user_name, :user_email => user_email, :message_body => message_body
+  end
 
 end
