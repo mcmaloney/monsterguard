6505bb18a105ca5245b435f40c4e0a3aa39124eb
Michael Maloney <maloney.mc@gmail.com>
2010-07-09 14:41:51 -0400

    changing support page

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,19 +1,5 @@
 class MainPagesController < ApplicationController
   def support
   end
-  
-  def send_support_message
-    if params[:support_message][:user_name].blank? || params[:support_message][:user_email].blank?
-      render :action => 'support'
-      flash[:notice] = "You must fill in all the required fields!"
-    else
-      SupportMailer.deliver_support_message(params[:support_message][:user_name], 
-                                            params[:support_message][:user_email], 
-                                            params[:support_message][:subject],
-                                            params[:support_message][:body])
-      render :action => 'support'
-      flash[:notice] = "Thanks for sending us a message! We'll get back to you as soon as we can."
-    end
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect '/support', :controller => 'main_pages', :action => 'support'
-  map.connect '/send_support_message', :controller => 'main_pages', :action => 'send_support_message', :conditions => { :method => :post }
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
